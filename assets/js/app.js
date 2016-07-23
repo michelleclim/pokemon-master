c0995f2667f1a3b8a09b4d252aa847c4c23d9ff3
Michelle Lim <michelle@edlio.com>
2016-07-23 00:53:24 -0700

    move pokemon data to service

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,25 +3,37 @@
 
 	var app = angular.module('pokemon-app', []);
 
+	app.service('pokemonDataService', ['$http', function($http){
+		var self = this;
+		var pokemonList = [];
+		var generatePokemon = generatePokemon;
+
+		function generatePokemon () {
+			var pokeId = Math.floor(Math.random()*150) + 1;
+			$http.get('https://pokeapi.co/api/v2/pokemon/' + pokeId)
+				.then(function(response){
+					pokemonList.push({
+						name: response.data.name,
+						id: response.data.id,
+						image: response.data.sprites.front_default,
+						value: ''
+					});
+				});
+		}
+
+		for (var i = 0; i < 10; i++) {
+			generatePokemon();
+		}
+
+		return pokemonList;
+	}]);
+
 	app.component('pokemonMaster', {
 		templateUrl: 'templates/pokemon-card.html',
-		controller: ['$http', function($http) {
+		controller: ['pokemonDataService', function(pokemonDataService) {
 			var self = this;
-			var pokemon;
-			self.pokemonList = [];
-			self.generatePokemon = generatePokemon;
+			self.pokemonList = pokemonDataService;
 
-			function generatePokemon () {
-				var pokeId = Math.floor(Math.random()*150) + 1;
-				$http.get('https://pokeapi.co/api/v2/pokemon/' + pokeId)
-					.then(function(response){
-						self.pokemonList.push(response.data);
-					});
-			}
-
-			for (var i = 0; i < 10; i++) {
-				generatePokemon();
-			}
 		}]
 	});
 

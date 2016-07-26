9296d112130a12d4cf5af49c221fcce726ff62a0
Michelle Lim <michelle@edlio.com>
2016-07-26 01:43:07 -0700

    add json file

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,36 +3,45 @@
 
 	var app = angular.module('pokemon-app', []);
 
-	app.service('pokemonDataService', ['$http', function($http){
+	app.service('pokemonDataService', ['$http', '$q', function($http, $q){
 		var self = this;
 		var pokemonList = [];
 		var generatePokemon = generatePokemon;
 
+		$http.get('/src/pokemon-with-images.json')
+			.then(function(response){
+				self.data = response.data.body;
+				for (var i = 0; i < 20; i++) {
+					generatePokemon();
+				}
+			});
+
 		function generatePokemon () {
 			var pokeId = Math.floor(Math.random()*150) + 1;
-			$http.get('https://pokeapi.co/api/v2/pokemon/' + pokeId)
-				.then(function(response){
-					pokemonList.push({
-						name: response.data.name,
-						id: response.data.id,
-						image: response.data.sprites.front_default,
-						value: ''
-					});
-				});
-		}
-
-		for (var i = 0; i < 10; i++) {
-			generatePokemon();
+			pokemonList.push(self.data[pokeId]);
 		}
 
 		return pokemonList;
 	}]);
 
-	app.component('pokemonMaster', {
+	app.service('pokemonIndex', function(){
+		this.index = 0;
+		this.updateIndex = updateIndex;
+
+		function updateIndex () {
+			this.index++;
+		}
+	});
+
+	app.component('pokemonCard', {
 		templateUrl: 'templates/pokemon-card.html',
-		controller: ['pokemonDataService', function(pokemonDataService) {
+		controller: ['pokemonDataService', 'pokemonIndex', '$timeout', function(pokemonDataService, pokemonIndex, $timeout) {
 			var self = this;
 			self.pokemonList = pokemonDataService;
+			self.pokemonIndex = pokemonIndex;
+			self.currentPokemon = self.pokemonList[self.pokemonIndex.index];
+
+			$timeout(function(){self.currentPokemon = self.pokemonList[0];}, 2000);
 
 		}]
 	});
